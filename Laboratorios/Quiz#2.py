320a0e679dbfeab165dabdb39a929dd94b518d53
Eloy2918 <ibra1890@hotmail.com>
2015-09-22 13:42:13 -0500

    Quiz#2

---
+++ Laboratorios/Quiz#2.py
@@ -0,0 +1,33 @@
+
+
+
+
+
+base = 5
+
+altura = 7
+
+area = base * altura
+
+perimetro = 2 * base + 2 * altura
+
+print (" El area del triangulo es de " + str (area))
+
+print (" El perimetro del triangulo es de " + str (perimetro))
+
+metrosa = area / 100
+
+metrosp = perimetro / 100 
+
+pulgadasa = area * 0.39370
+
+pulgadasp = perimetro * 0.39370
+
+print (" La conversion del area del triangulo de cm a metros es " + str (metrosa))
+
+print (" La conversion del perimetro del triangulo de cm a metros es " + str (metrosp))
+
+print (" La conversion del area del triangulo de cm a a pulgadas es " + str (pulgadasa))
+
+print (" La conversion del perimetro del triangulo de cm a a pulgadas es " + str (pulgadasp))
+
